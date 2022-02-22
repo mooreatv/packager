73a1e6a6ea9b98e6b14f203c1ca9f4e1684879cb
mooreatv <46745059+mooreatv@users.noreply.github.com>
2022-02-22 15:04:06 -0800

    Adding "current" game interface version and small script to update (multi)tocs according to these values  (#2)
    Note that this isn't supporting single toc with multiple interface lines and also assume your "main" toc is either a symlink or copied during build from -Mainline.toc (as it isn't updated by the script)
    
    * adding interfaces
    * update all found toc with current interface version
    * show which files are looked at
    * retail is now 9.2.0

---
+++ update-versions.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# Find your multi toc and update them
+
+# shellcheck source=interfaces.txt
+source "$(dirname "${BASH_SOURCE[0]}")/interfaces.txt"
+
+variants="BCC Classic Mainline"
+
+for v in $variants; do
+    echo "Toc for $v is ${!v}"
+done
+
+for v in $variants; do
+    find . -type f -name "*-$v.toc" -print0 | xargs -0 -I % sh -c "echo 'Updating %'; sed -i -E -e \"s/## *Interface:.*/## Interface: ${!v}/i\" %"
+done
